4051ea8bb430ad05074f403db5f962c1e762e171
hyeucong <94946649+hyeucong@users.noreply.github.com>
2026-04-02 02:42:09 +0700

    improve ui

--- resources/js/pages/assets/index.tsx
+++ resources/js/pages/assets/index.tsx
@@ -1,11 +1,38 @@
 import { Head } from '@inertiajs/react';
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table"
 
 export default function Assets() {
     return (
         <>
-            <Head title="Dashboard" />
+            <Head title="Assets" />
             <div className="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4">
-
+                <Table>
+                    <TableHeader>
+                        <TableRow>
+                            <TableHead className="w-25">Invoice</TableHead>
+                            <TableHead>Status</TableHead>
+                            <TableHead>Method</TableHead>
+                            <TableHead className="text-right">Amount</TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                        <TableRow>
+                            <TableCell className="font-medium">INV001</TableCell>
+                            <TableCell>Paid</TableCell>
+                            <TableCell>Credit Card</TableCell>
+                            <TableCell className="text-right">$250.00</TableCell>
+                        </TableRow>
+                    </TableBody>
+                    <TableCaption>A list of your recent invoices.</TableCaption>
+                </Table>
             </div>
         </>
     );
